01b2f7d2890bde911af5958e4523f959b15f63e0
rithnarin <rithnarin@yahoo.com>
2017-11-29 11:17:02 -0800

    Update RoomList.jsx
    Add PropTypes

--- client/src/components/RoomList.jsx
+++ client/src/components/RoomList.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import RoomListEntry from './RoomListEntry';
 
-const RoomList = (props) => (
+const RoomList = ({rooms, changeView}) => (
   <ul>
-    {props.rooms.map(room => <RoomListEntry room={room} changeView={props.changeView} />)}
+    {rooms.map(room => <RoomListEntry room={room} changeView={changeView} />)}
   </ul>
 );
 
 export default RoomList;
+
+
+RoomList.propTypes = {
+  rooms: PropTypes.arrayOf(PropTypes.string).isRequired,
+  changeView: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
